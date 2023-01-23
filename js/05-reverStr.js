2d8f44274858b7cb44c55a23c3c6331786d3affa
Mindaugas <mindaugas.paulius0@gmail.com>
2023-01-23 14:56:34 +0200

    05 update

--- js/05-reverStr.js
+++ js/05-reverStr.js
@@ -8,3 +8,10 @@ function solution(str){
 }
 
 console.log(solution('vsdfdgdv'));
+
+// ---------------------------
+// function solution(str){
+//   return str.split('').reverse().join('');  
+// }
+// --------------------------
+// const solution = str => str.split('').reverse().join('');
